056675aac5cf16496e888d1c20400dbebc619ab7
Jasmine Gadie and Megan Lee <jasmine@>
2018-02-21 20:56:49 -0800

    added a button submission rather than prompt, and created a hidden div for follow-up questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
-$(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
+// $(document).ready(function() {
+//   $("#agebutton").submit(function(event) {
+//   var age = parseInt($("age-first")).val();
+//   }
+//   $(".result").show();
+//
+//   if (age > 21) {
+//     $('.celebrity').show();
+//
+//   } else {
+//     $('.under-21').show();
+//   }
+//
+// event.preventDefault();
+// });
 
-  if (age > 21) {
-    $('#celebrity').show();
 
-  } else {
-    $('#under-21').show();
-  }
+$(document).ready(function(){
+  $("#agebutton").submit(function(event){
+    event.preventDefault();
+
+    //user input variable
+    var age = parseInt($("age-first").val());
+
+    //this shows the second form
+    $(".result").show();
+
+    //shows a new section of html based on the user's age inputted
+    if (age > 21) {
+      $('.celebrity').show();
+
+    } else {
+      $('.under-21').show();
+    }
+
+  });
 });
